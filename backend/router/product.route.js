bb712592daf80008b2c27fa706ad1a1cdba08cfa
Allenpt221 <Allenpt26@gmail.com>
2025-06-23 15:42:28 +0800

    update the routers

--- backend/router/product.route.js
+++ backend/router/product.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createProduct } from '../control/product.control.js';
+import { createProduct, deleteProduct, getAllProduct } from '../control/product.control.js';
 import { adminRoute, protectRoute } from '../middleware/auth.middleware.js';
 
 const route = express.Router();
 
 
-route.post('/', protectRoute, adminRoute ,createProduct);
+route.post('/createproduct', protectRoute, adminRoute ,createProduct);
+route.get('/', getAllProduct);
+route.delete('/:id', deleteProduct);
 
 export default route;
