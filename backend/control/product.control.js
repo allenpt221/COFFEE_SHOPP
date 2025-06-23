2f46918caf990c73ca381250a6b89878074668ff
Allenpt221 <Allenpt26@gmail.com>
2025-06-23 15:41:06 +0800

    create the get all the product and also create the cloudinary for saving the image

--- backend/control/product.control.js
+++ backend/control/product.control.js
@@ -1,14 +1,33 @@
+import cloudinary from "../lib/cloudinary.js";
 import Product from "../model/product.model.js";
+
+
+export const getAllProduct = async (req, res) => {
+    try {
+        const product = await Product.find({});
+
+        res.status(200).json({product});
+    } catch (error) {
+        console.log("Error in getAllProduct controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
 
 export const createProduct = async (req, res) => {
     try {
         const { name, description, price, image, category } = req.body;
 
+        let cloudinaryResponse = null;
+
+        if (image){
+            cloudinaryResponse = await cloudinary.uploader.upload(image, { folder: "products" });
+        }
+        
         const product = await Product.create({
 			name,
 			description,
 			price,
-			image,
+			image: cloudinaryResponse ? cloudinaryResponse.secure_url : "",
 			category,
 		});
 
@@ -18,3 +37,17 @@ export const createProduct = async (req, res) => {
 		res.status(500).json({ message: "Server error", error: error.message });
     }
 };
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        
+        
+
+        await Product.findByIdAndDelete(req.params.id);
+
+    } catch (error) {
+        console.log("Error in deleteProduct controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
