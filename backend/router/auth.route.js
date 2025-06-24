b8066f128293a31b10061b44442d3e7b25c67c25
Allenpt221 <Allenpt26@gmail.com>
2025-06-24 10:14:02 +0800

    get use profile

--- backend/router/auth.route.js
+++ backend/router/auth.route.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import { logIn, logout, signUp } from '../control/auth.control.js';
+import { getProfile, logIn, logout, refreshToken, signUp } from '../control/auth.control.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 
 router.post('/signup', signUp);
 router.post('/login', logIn);
-router.post('/logut', logout);
+router.post('/logout', logout);
+router.post("/refresh-token", refreshToken);
+router.get("/profile", protectRoute, getProfile);
 
 
 export default router;
