99f35e18d39374b833dcc4fb32cf0825b89de043
Allenpt221 <Allenpt26@gmail.com>
2025-07-15 19:59:11 +0800

    create some footer for the webiste and add some framer motion animation into the home page

---
+++ frontend/src/Pages/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='max-w-4xl mx-auto'>
+        <div>
+            <img src='/Image/KPT.webp' alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,9 +5,12 @@ import { Link } from 'react-router-dom';
 import Slider from '../components/Slider';
 import BestSelling from '@/components/BestSelling';
 
-import cafeChoose from '/Image/chooseCafe.png'
-import cafeOrder from '/Image/orderCafe.png'
-import cafeDelivery from '/Image/deliveryCafe.png'
+import cafeChoose from '/Image/chooseCafe.png';
+import cafeOrder from '/Image/orderCafe.png';
+import cafeDelivery from '/Image/deliveryCafe.png';
+
+import { motion } from 'framer-motion';
+import Footer from './Footer';
 
 
 
@@ -35,31 +38,56 @@ const Home = () => {
  
 
   return (
-    <div className="space-y-10 min-h-screen">
+    <motion.div 
+    className="space-y-10 min-h-screen">
       {/* Top Hero Section */}
       <div className='relative overflow-hidden'>
         <div className='bg-[#66510b1e] md:h-[7rem] h-[5rem]'>
-          <img src="/image/GrabDrink.png" alt="" className='absolute sm:left-10 md:top-5 left-2 lg:w-[50rem] md:w-[40rem] sm:w-[30rem] w-[19rem] sm:h-[5rem] h-[5rem]' />
+          <motion.img 
+          initial={{opacity: 0, y: 0}}
+          animate={{opacity: 1, y: 0}}
+          transition={{ duration: 0.8}}
+          src="/image/GrabDrink.png" alt="" className='absolute sm:left-10 md:top-5 left-2 lg:w-[50rem] md:w-[40rem] sm:w-[30rem] w-[19rem] sm:h-[5rem] h-[5rem]' />
         </div>
         <img src="/image/BackGround.png" alt="" className='h-[25rem] w-full object-cover' />
-        <div className='absolute md:top-50 sm:top-[6rem] lg:w-[30rem] md:w-[14rem] sm:w-[12rem] w-[12rem] md:left-80 sm:left-[20rem] left-[7.5rem] top-[6rem] font-medium text-base'>
+        <motion.div 
+        initial={{opacity: 0, y: 0}}
+        animate={{opacity: 1, y: 0}}
+        transition={{ duration: 0.8}}
+        className='absolute md:top-50 sm:top-[6rem] lg:w-[30rem] md:w-[14rem] sm:w-[12rem] w-[12rem] md:left-80 sm:left-[20rem] left-[7.5rem] top-[6rem] font-medium text-base'>
           <p>Your refreshment is chilled and ready to cool you down.
             Bold flavors, perfectly balanced exactly how you like it.
             Pick it up and sip into something great.
           </p>
-        </div>
-        <div className='absolute lg:top-[18rem] md:top-[22rem] sm:top-[17rem] top-[27rem] sm:left-[20rem]  left-[8.5rem] float-right'>
+        </motion.div>
+        <motion.div 
+        initial={{opacity: 0, y: 0}}
+        animate={{opacity: 1, y: 0}}
+        transition={{ duration: 0.5}}
+        className='absolute lg:top-[18rem] md:top-[22rem] sm:top-[17rem] top-[27rem] sm:left-[20rem]  left-[8.5rem] float-right'>
           <Link to="/menu" className='cursor-pointer px-2 border flex gap-2 bg-[#d3d3d3b4] hover:scale-102 active:scale-95 font-medium py-1 rounded-lg items-center'>
             <p>Explore Our Menu</p>
             <MoveRight size={23} />
           </Link>
-        </div>
-        <img src="/image/cafeHomeImage.png" alt="" className='absolute top-0 right-0 sm:h-[37rem] md:block hidden' />
-        <img src="/image/coffeeHome.png" alt="" className='absolute md:top-44 top-20 sm:left-0 -left-35 md:h-[30rem]' />
+        </motion.div>
+        <motion.img 
+        initial={{opacity: 0, y: -25}}
+        animate={{opacity: 1, y: 0}}
+        transition={{ duration: 0.5}}
+        src="/image/cafeHomeImage.png" alt="" className='absolute top-0 right-0 sm:h-[37rem] md:block hidden' />
+        <motion.img 
+        initial={{opacity: 0, y: -25}}
+        animate={{opacity: 1, y: 0}}
+        transition={{ duration: 0.5}}
+        src="/image/coffeeHome.png" alt="" className='absolute md:top-44 top-20 sm:left-0 -left-35 md:h-[30rem]' />
       </div>
 
       {/* Title */}
-      <section className='space-y-8'>
+      <motion.section 
+      initial={{opacity: 0, y: -25}}
+      animate={{opacity: 1, y: 0}}
+      transition={{ duration: 0.5}}
+      className='space-y-8'>
         <p className=' sm:mx-30 mx-0 lg:text-start text-center font-bold md:text-4xl text-2xl'>Our Best Offers for You</p>
         {/* Draggable Offer Slider */}
         <div className='max-w-7xl mx-auto'>
@@ -104,8 +132,12 @@ const Home = () => {
             ))}
           </div>
         </div>
-      </section>
-    </div>
+      </motion.section>
+    
+      <div>
+        <Footer />
+      </div>
+    </motion.div>
   );
 };
 
