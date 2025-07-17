ee177f22d46dfa16644b8789781c29a2e1387b80
Allenpt221 <Allenpt26@gmail.com>
2025-07-18 03:29:18 +0800

    ...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -12,11 +11,12 @@ export default defineConfig({
     },
   },
   server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-		
-}
-});
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
