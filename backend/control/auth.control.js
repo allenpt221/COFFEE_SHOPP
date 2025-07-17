96f02a4b492f4f27c1181961e21d2a48ee162bc1
Allenpt221 <Allenpt26@gmail.com>
2025-07-17 21:28:10 +0800

    remove the strict in token

--- backend/control/auth.control.js
+++ backend/control/auth.control.js
@@ -30,7 +30,7 @@ const setCookies = (res, accessToken, refreshToken) => {
 	res.cookie("refreshToken", refreshToken, {
 		httpOnly: true, // prevent XSS attacks, cross site scripting attack
 		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+		sameSite: isProduction ? "None" : "Lax", // prevents CSRF attack, cross-site request forgery attack
 		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 	});
 };
