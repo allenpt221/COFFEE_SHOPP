ff95df0b2c748be29afed4130b337c3252def554
Allenpt221 <Allenpt26@gmail.com>
2025-07-17 22:57:12 +0800

    ...

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://coffee-shopp.onrender.com/api" : "/api",
-	withCredentials: true, // send cookies to the server
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://coffee-shopp.onrender.com/api",
+  withCredentials: true, // important for cookies
 });
 
 export default axiosInstance;
