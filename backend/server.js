a76a538a0fe5bb82da9930b5f133daa900c477ca
Allenpt221 <Allenpt26@gmail.com>
2025-07-18 02:41:54 +0800

    ...

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from "cors"
+import path from "path";
+
 import { connectDB } from './lib/db.js';
 
 import cookieParser from 'cookie-parser';
@@ -24,7 +26,7 @@ app.use(cors({
 }));
 
 
-
+const __dirname = path.resolve();
 
 
 
@@ -42,9 +44,13 @@ app.use('/api/cartproduct', cartRoutes);
 app.use('/api/orders', orderRoutes);
 
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-
-
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port  http://localhost:${PORT}`);
