5e9671a60578b9f5b8d3162a9223a40da4ac3924
Allenpt221 <Allenpt26@gmail.com>
2025-07-19 03:09:00 +0800

    some changes

--- backend/server.js
+++ backend/server.js
@@ -40,13 +40,13 @@ app.use('/api/orders', orderRoutes);
 
 // Static file serving
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
-
+  
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   connectDB();
