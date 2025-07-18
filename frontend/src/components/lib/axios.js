08bceea99a2c723704160033c3146698d98475a6
Allenpt221 <Allenpt26@gmail.com>
2025-07-19 05:42:56 +0800

    changessss

--- frontend/src/components/lib/axios.js
+++ frontend/src/components/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "https://coffee-shopp.onrender.com/api",
+	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "https://coffee-shopp-2.onrender.com/api",
 	withCredentials: true, // send cookies to the server
 });
 
