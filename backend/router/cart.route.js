34351ce1e694bbdfebc7246d652ac689fa197971
Allenpt221 <Allenpt26@gmail.com>
2025-07-19 03:20:38 +0800

    changes for debuging purpose

--- backend/router/cart.route.js
+++ backend/router/cart.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/", protectRoute, getcartProducts);
 router.post("/", protectRoute, addToCart);
 router.delete("/", protectRoute, removeAllFromCart);
-router.put("/:id", protectRoute, updateQuantity);
+// router.put("/:id", protectRoute, updateQuantity);
 
 
 export default router;
