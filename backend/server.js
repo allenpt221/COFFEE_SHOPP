6d49c18aa55176cadf3a09df3df6ea295e3a889e
Allenpt221 <Allenpt26@gmail.com>
2025-07-19 03:03:23 +0800

    some changes

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,12 @@ app.use('/api/orders', orderRoutes);
 
 
 // Static file serving
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 
 app.listen(PORT, () => {
