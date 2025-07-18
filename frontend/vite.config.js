17e86a968ff1de8fff9d6fcd777222de397836b4
Allenpt221 <Allenpt26@gmail.com>
2025-07-19 04:02:55 +0800

    changes in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss() // Official Tailwind Vite plugin for v4.1
+  ],
   server: {
     proxy: {
-      "/api" : {
-        target: "http://localhost:5000",
-      },
-    },
-  },
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
