066f02ae93d5d5fc349eed5d481fdadc245686e8
Allenpt221 <Allenpt26@gmail.com>
2025-07-19 13:12:56 +0800

    new changes for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // ✅ make sure Vite outputs to ./frontend/dist
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5000', // only applies locally
         changeOrigin: true,
         secure: false,
       },
