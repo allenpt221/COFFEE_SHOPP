fa3600ca7ad9eaf729dcd6d39cd41d96fe49f0e9
Allenpt221 <Allenpt26@gmail.com>
2025-07-19 13:00:39 +0800

    changesss

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ if(process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
 // ✅ Corrected SPA Fallback
-app.get(/^(?!\/api).*/, (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
 }
