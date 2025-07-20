1542e9bec84a538bb4898611f937ecb13777c71d
Allenpt221 <Allenpt26@gmail.com>
2025-07-20 14:56:17 +0800

    try this

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,12 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 // Middleware
-app.use(cors({
-  origin: [
-    process.env.CLIENT_URL,
-    'http://localhost:5173'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: [
+//     process.env.CLIENT_URL,
+//     'http://localhost:5173'],
+//   credentials: true
+// }));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
