4d4537a76eee0b6f6daa6791e64727a00bbc969f
Allenpt221 <Allenpt26@gmail.com>
2025-07-20 15:32:42 +0800

    changess

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,13 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-
 import authRouter from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
 import orderRoutes from './routes/order.route.js';
 import { connectDB } from './lib/db.js';
 
-// Load env variables
+// Load environment variables
 dotenv.config();
 
 const app = express();
@@ -22,14 +21,13 @@ const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(__filename);
-console.log(__dirname);
 
 // Middleware
 app.use(cors({
   origin: [
     process.env.CLIENT_URL,
-    'http://localhost:5173'],
+    'http://localhost:5173'
+  ],
   credentials: true
 }));
 
@@ -37,21 +35,18 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 
 // API Routes
-app.get('/', (req, res) => res.send('Server is running...'));
+app.get('/api', (req, res) => res.send('API is running...'));
 app.use('/api/auth', authRouter);
 app.use('/api/product', productRoutes);
 app.use('/api/cartproduct', cartRoutes);
 app.use('/api/orders', orderRoutes);
 
-// Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.resolve(__dirname, '../frontend/dist');
-  app.use(express.static(frontendPath));
+const frontendPath = path.resolve(__dirname, '../frontend/dist');
+app.use(express.static(frontendPath));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(frontendPath, 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
+});
 
 // Start server
 connectDB().then(() => {
